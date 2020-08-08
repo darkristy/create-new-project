93f478339e5648796e70e6d790d58040cbb8de39
Kahlil Whitfield <darkristy2002@gmail.com>
2020-08-08 07:06:30 -0400

    :construction: CLI in progress - Minor Changes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = {
   getCurrentDirectory: process.cwd(),
-  exists: async filePath => fs.existsSync(filePath),
+  exists: async filePath => !!fs.existsSync(filePath),
   createNewDirectory: async projectPath => fs.mkdirSync(projectPath),
   changeToNewDirectory: async projectPath => process.chdir(projectPath),
 };
