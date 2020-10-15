0cb4a09bc7ea3fcf51972d612a55b284ef36baf0
Kahlil Whitifield <darkristy2002@gmail.com>
2020-10-15 13:18:02 -0400

    Temp Removing Args

--- src/cli.js
+++ src/cli.js
@@ -7,11 +7,11 @@ const yargs = require('yargs');
 const taskLists = require('./tasks');
 
 const { argv } = yargs.options({
-  username: {
-    alias: 'user',
-    description: 'Give the CLI your username.',
-    demandOption: true,
-  },
+  // username: {
+  //   alias: 'user',
+  //   description: 'Give the CLI your username.',
+  //   demandOption: true,
+  // },
 });
 
 inquirer
